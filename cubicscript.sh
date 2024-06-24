274a605ebbe0207d077a79b5235b9edea1174065
oliveiraezequiel <ezequieldeoliveira486@gmail.com>
2024-06-23 21:19:03 -0300

    Add files via upload

---
+++ cubicscript.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+Instale o Cubic no Ubuntu
+
+
+sudo apt-add-repository ppa:cubic-wizard/release -y
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 6494C6D6997C215E -y
+sudo apt update && sudo apt install cubic -y
+echo "o grupo EDX linux e trianguloOS agradecem a todos"
+echo "tenham um bom dia"
